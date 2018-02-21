5d2c5c264b7088204947fc00bab9dcfada02d944
konstantin0s <constantintofan85@gmail>
2018-02-21 15:51:59 +0100

    Add keyup and keydown arrow functionality

--- js/pong.js
+++ js/pong.js
@@ -1,6 +1,8 @@
 
 
 var WIDTH=700, HEIGHT=600, pi=Math.PI;
+var UpArrow=38, DownArrow=40;
+
 var canvas, ctx, keystate;
 var player, ai, ball;
 
@@ -10,7 +12,10 @@ player = {
   width: 20,
   height: 100,
 
-  update: function() {},
+  update: function() {
+    if (keystate[UpArrow]) this.y -= 7;
+    if (keystate[DownArrow]) this.y += 7;
+  },
   draw: function () {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
@@ -45,6 +50,15 @@ function main() {
   canvas.height = HEIGHT;
   ctx = canvas.getContext("2d");
   document.body.appendChild(canvas);
+
+  keystate = {};
+  document.addEventListener("keydown", function(evt) {
+    keystate[evt.keyCode] = true;
+  });
+
+  document.addEventListener("keyup", function(evt) {
+    delete keystate[evt.keyCode];
+  });
 
   init();
 
@@ -87,7 +101,7 @@ ai.draw();
 var w = 4;
 var x = (WIDTH -w)*0.5;
 var y = 0;
-var step = HEIGHT/15;
+var step = HEIGHT/20;
 while(y < HEIGHT) {
   ctx.fillRect(x, y+step*0.25, w, step*0.5);
   y += step;
